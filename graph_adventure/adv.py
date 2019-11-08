e38fa9dc4e86da39ba7d8a398f761c53ae6581cf
icpengineer <icpengineer@intcrzyppl.com>
2019-11-08 16:13:28 +0100

    inserted queue and stack

--- graph_adventure/adv.py
+++ graph_adventure/adv.py
@@ -1,8 +1,33 @@
 from room import Room
 from player import Player
 from world import World
-from util import Stack, Queue
+# from util import Stack, Queue
 
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
 import random
 
 # Load world
